06bf8d0cc081026499c2d91f7665a900bfd91cec
Joel O'Connor <joconno07@gmail.com>
2018-11-06 10:14:00 -0700

    Built out fetch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import { apiKey } from '../apiKey';
 
 Vue.config.productionTip = false
 
 new Vue({
+  data: {
+    astroData: {}
+  },
+  mounded() {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=PpsMSUDtp43F9KeswYphKmlJFArYNrZycwBT36qv')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  },
   render: h => h(App),
 }).$mount('#app')
