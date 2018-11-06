9a63c6acb4ea6785f3866730ba7c85723c885d32
Joel O'Connor <joconno07@gmail.com>
2018-11-06 12:03:18 -0700

    Handel merge conflicts

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 import Vue from 'vue'
-import { apiKey } from './apiKey.js';
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
 new Vue({
-<<<<<<< HEAD
-  el: '#app',
-  data: {
-    astroData: 'show this info'
-  },
-  mounted() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
-      .then(response => response.json())
-      .then(data => this.astroData = data);
-  }
-})
-=======
   render: h => h(App)
 }).$mount('#app')
->>>>>>> 365e2b713bbeeef231de53d5f87e4652af0a2791
